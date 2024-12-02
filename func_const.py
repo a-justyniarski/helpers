77647a7236af7ce5dea6e667323d24fa0524fd91
unknown <ajustyniarski@gmail.com>
2024-12-02 10:15:00 +0100

    init

---
+++ func_const.py
@@ -0,0 +1,7 @@
+class Months:
+	TRANSLATE_POLISH_TO_ENGLISH_FULL: dict = {'styczeń'    : 1, 'luty': 2, 'marzec': 3, 'kwiecień': 4, 'maj': 5,
+											  'czerwiec'   : 6, 'lipiec': 7, 'sierpień': 8, 'wrzesień': 9,
+											  'październik': 10, 'listopad': 11, 'grudzień': 12, }
+	TRANSLATE_POLISH_TO_ENGLISH_FULL_CONJ: dict = {'stycznia'    : 1, 'lutego': 2, 'marca': 3, 'kwietnia': 4, 'maja': 5,
+												   'czerwca'     : 6, 'lipca': 7, 'sierpnia': 8, 'września': 9,
+												   'października': 10, 'listopada': 11, 'grudnia': 12, }
